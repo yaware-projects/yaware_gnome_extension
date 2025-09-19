a308ef5153c7868ce81222a15f03e3b7d9ef4179
Mykhailo_Ametov <m.ametov@yaware.com>
2025-09-19 11:19:19 +0300

    Add extension.js, metadata.json, update readme.md

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# yaware_gnome_extension
-Yaware.TimeTracker extension for Gnome Shell
+# Yaware.TimeTracker extension for Gnome Shell
+
+This is a companion extension for the [Yaware](https://yaware.com/) desktop time-tracking application. It acts as a bridge, allowing the main application to securely access information from the GNOME Shell desktop environment, which is necessary for accurate activity tracking.
+
+This extension has no visible user interface and works in the background, responding to requests only from the Yaware client.
+
+### Features
+This extension provides a D-Bus service (org.gnome.Shell.Extensions.Yaware) that enables the following functionalities for the Yaware desktop client:
+
+* List Windows: Provides a complete list of all currently open windows, including their titles, process IDs, and application classes.
+
+* Identify Active Window: Determines which window is currently in focus to track active application usage accurately.
+
+* Take Screenshots: Allows the Yaware client to request screenshots of the desktop for reporting purposes. The extension handles the screen capture securely and passes the resulting image path back to the client.
---
+++ extension.js
@@ -0,0 +1,85 @@
+const { Gio, Shell, GLib } = imports.gi;
+
+Gio._promisify(Shell.Screenshot.prototype, 'pick_color');
+Gio._promisify(Shell.Screenshot.prototype, 'screenshot');
+Gio._promisify(Shell.Screenshot.prototype, 'screenshot_window');
+Gio._promisify(Shell.Screenshot.prototype, 'screenshot_area');
+Gio._promisify(Shell.Screenshot.prototype, 'screenshot_stage_to_content');
+Gio._promisify(Shell.Screenshot, 'composite_to_stream');
+
+import {Extension, gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
+
+const MR_DBUS_IFACE = `
+<node>
+    <interface name="org.gnome.Shell.Extensions.Yaware">
+        <method name="ListWindow">
+            <arg type="s" direction="out" name="windows"/>
+        </method>
+        <method name="ActiveWindow">
+            <arg type="s" direction="out" name="window"/>
+        </method>
+        <method name="TakeScreenshot">
+            <arg type="b" direction="in" name="includeCursor"/>
+            <arg type="s" direction="in" name="inputPath"/>
+            <arg type="b" direction="out" name="result"/>
+        </method>
+    </interface>
+</node>`;
+
+export default class GrabWindowInfoExtension extends Extension {
+    enable() {
+        this._dbus = Gio.DBusExportedObject.wrapJSObject(MR_DBUS_IFACE, this);
+        this._dbus.export(Gio.DBus.session, '/org/gnome/Shell/Extensions/Yaware');
+    }
+
+    disable() {
+        if (this._dbus) {
+            this._dbus.flush();
+            this._dbus.unexport();
+            delete this._dbus;
+        }
+    }
+
+    ListWindow() {
+        let win = global.get_window_actors()
+            .map(a => a.meta_window)
+            .map(w => ({
+                id: w.get_id(),
+                pid: w.get_pid(),
+                class: w.get_wm_class(),
+                title: w.get_title(),
+                focus: w.has_focus()
+            }));
+        return JSON.stringify(win);
+    }
+
+    ActiveWindow() {
+        let win = global.get_window_actors()
+            .map(a => a.meta_window)
+            .map(w => ({
+                id: w.get_id(),
+                pid: w.get_pid(),
+                class: w.get_wm_class(),
+                title: w.get_title(),
+                focus: w.has_focus()
+            }))
+            .filter(w => w.focus === true);
+        return JSON.stringify(win);
+    }
+
+    async TakeScreenshot(includeCursor, inputPath) {
+        if (inputPath == '')
+            return false;
+
+        let shooter = new Shell.Screenshot();
+        if (!shooter)
+            return false;
+
+        let file = Gio.File.new_for_path(inputPath);
+        let stream = file.replace(null, false, Gio.FileCreateFlags.NONE, null);
+        let [area] = await shooter.screenshot(includeCursor, stream);
+        stream.close(null);
+        return true;
+    }
+}
+
